a19f6b6341496383204e9db2ff853fe644c7033e
oleg <olegarxo15@gmail.com>
2022-07-07 17:36:18 +0300

    Сделал первое ДЗ

---
+++ homework_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите A");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите B");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int parseMassive(int A, int B)
+{
+      int number = Convert.ToInt32(Math.Pow(A,B));
+      return number;
+}
+int tamp = parseMassive(number,numberSecond);
+Console.WriteLine(tamp);
