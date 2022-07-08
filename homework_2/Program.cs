0b767ecdecb69ed244b9c22f9fc89f9b37db609f
oleg <olegarxo15@gmail.com>
2022-07-08 11:04:35 +0300

    Сделал второе ДЗ

---
+++ homework_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ВВедите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int sumnumber(int number)
+{
+    int sum = 0;
+    while(number > 0)
+        {
+            sum += number % 10;
+            number /=10;
+        }
+    return sum;
+}
+sum = sumnumber(number);
+Console.WriteLine(sum);
